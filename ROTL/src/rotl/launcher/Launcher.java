1d3ed811e3bead890f2b0bda1d16ccab1d48e29a
Lupascu Marian <marianlupascu15@gmail.com>
2018-03-13 11:11:32 +0200

    Update Launcher.java

--- ROTL/src/rotl/launcher/Launcher.java
+++ ROTL/src/rotl/launcher/Launcher.java
@@ -3,7 +3,7 @@ package rotl.launcher;
 import rotl.game.Game;
 
 public class Launcher {
-
+		//comentariuuuuuuuuu
 	public static void main(String args[]){
 		Game game = new Game("ROTL");
 		game.start();
