2eeaa7fae725e78c1c5afe06eee32b9f330783b1
marianlupascu <marianlupascu15@gmail.com>
2018-06-08 08:54:35 +0300

    ReadmeV5 :lips:

--- src/rotl/menu/Settings.java
+++ src/rotl/menu/Settings.java
@@ -28,13 +28,13 @@ public final class Settings extends JPanel implements MenuOption {
 
 	private static int closeImgDimensionsX;
 	private static int closeImgDimensionsY;
-	
+
 	private static int volumeIconDimensionsX;
 	private static int volumeIconDimensionsY;
-	
+
 	private static List<Integer> volumeIconPosX = new ArrayList<>();
 	private static List<Integer> volumeIconPosY = new ArrayList<>();
-	
+
 	private static Point closeImgPosition = new Point();
 
 	private static Handler handler;
@@ -48,7 +48,7 @@ public final class Settings extends JPanel implements MenuOption {
 
 	private static BufferedImage volumeIcon;
 	private static BufferedImage muteIcon;
-	
+
 	private Settings(Handler handler) {
 
 		Settings.handler = handler;
@@ -84,7 +84,7 @@ public final class Settings extends JPanel implements MenuOption {
 
 			@Override
 			public void focusLost(FocusEvent arg0) {
-				
+
 				frame.setVisible(false);
 			}
 		});
@@ -105,34 +105,40 @@ public final class Settings extends JPanel implements MenuOption {
 		super.paintComponent(g);
 
 		g.drawImage(backgroundImg, 0, 0, screenWidth, screenHeight, this);
-		
+
 		g.setFont(new Font("Neuropol X", Font.BOLD, titleFontSize));
 		g.setColor(Color.WHITE);
 		g.drawString("Settings", (int) (screenWidth * 35 / 100), (int) (screenHeight * 15 / 100));
-		
-		g.setFont(new Font("Neuropol X", Font.BOLD, 55));		
-							
-		g.drawString("Menu Music : ", (int) (screenWidth * 10 / 100), (int) (screenHeight * 40 / 100));		
-		g.drawString("Game Music : ", (int) (screenWidth * 10 / 100), (int) (screenHeight * 60 / 100));		
+
+		g.setFont(new Font("Neuropol X", Font.BOLD, 55));
+
+		g.drawString("Menu Music : ", (int) (screenWidth * 10 / 100), (int) (screenHeight * 40 / 100));
+		g.drawString("Game Music : ", (int) (screenWidth * 10 / 100), (int) (screenHeight * 60 / 100));
 		g.drawString("Arena/Store Music : ", (int) (screenWidth * 10 / 100), (int) (screenHeight * 80 / 100));
-		
+
 		final Sounds sounds = Sounds.getInstance();
-		
-		if (!sounds.isMenuMusicMuted())	
-			g.drawImage(volumeIcon, volumeIconPosX.get(0), volumeIconPosY.get(0), volumeIconDimensionsX, volumeIconDimensionsY, this);
+
+		if (!sounds.isMenuMusicMuted())
+			g.drawImage(volumeIcon, volumeIconPosX.get(0), volumeIconPosY.get(0), volumeIconDimensionsX,
+					volumeIconDimensionsY, this);
 		else
-			g.drawImage(muteIcon, volumeIconPosX.get(0), volumeIconPosY.get(0), volumeIconDimensionsX, volumeIconDimensionsY, this);
-		
+			g.drawImage(muteIcon, volumeIconPosX.get(0), volumeIconPosY.get(0), volumeIconDimensionsX,
+					volumeIconDimensionsY, this);
+
 		if (!sounds.isGameMusicMuted())
-			g.drawImage(volumeIcon, volumeIconPosX.get(1), volumeIconPosY.get(1), volumeIconDimensionsX, volumeIconDimensionsY, this);
+			g.drawImage(volumeIcon, volumeIconPosX.get(1), volumeIconPosY.get(1), volumeIconDimensionsX,
+					volumeIconDimensionsY, this);
 		else
-			g.drawImage(muteIcon, volumeIconPosX.get(1), volumeIconPosY.get(1), volumeIconDimensionsX, volumeIconDimensionsY, this);
-		
+			g.drawImage(muteIcon, volumeIconPosX.get(1), volumeIconPosY.get(1), volumeIconDimensionsX,
+					volumeIconDimensionsY, this);
+
 		if (!sounds.isBattleMusicMuted())
-			g.drawImage(volumeIcon, volumeIconPosX.get(2) + 10, volumeIconPosY.get(2), volumeIconDimensionsX, volumeIconDimensionsY, this);
+			g.drawImage(volumeIcon, volumeIconPosX.get(2) + 15, volumeIconPosY.get(2), volumeIconDimensionsX,
+					volumeIconDimensionsY, this);
 		else
-			g.drawImage(muteIcon, volumeIconPosX.get(2) + 10, volumeIconPosY.get(2), volumeIconDimensionsX, volumeIconDimensionsY, this);
-		
+			g.drawImage(muteIcon, volumeIconPosX.get(2) + 15, volumeIconPosY.get(2), volumeIconDimensionsX,
+					volumeIconDimensionsY, this);
+
 		g.drawImage(closeImg, closeImgPosition.x, closeImgPosition.y, closeImgDimensionsX, closeImgDimensionsY, this);
 	}
 
@@ -141,27 +147,27 @@ public final class Settings extends JPanel implements MenuOption {
 		closeImgDimensionsX = (int) (screenWidth * 5.5 / 100);
 		closeImgDimensionsY = (int) (screenHeight * 9.8 / 100);
 		closeImgPosition.setLocation(screenWidth - closeImgDimensionsX, 0);
-		
-		volumeIconDimensionsX = (int) (screenWidth * 7 / 100);		
-		volumeIconDimensionsY = (int) (screenHeight * 11 / 100);		
-		
-		//X's for 3 Volume Icons
-		volumeIconPosX.add((int) (screenWidth * 43 / 100));		
-		volumeIconPosX.add((int) (screenWidth * 43 / 100));		
+
+		volumeIconDimensionsX = (int) (screenWidth * 7 / 100);
+		volumeIconDimensionsY = (int) (screenHeight * 11 / 100);
+
+		// X's for 3 Volume Icons
+		volumeIconPosX.add((int) (screenWidth * 43 / 100));
+		volumeIconPosX.add((int) (screenWidth * 43 / 100));
 		volumeIconPosX.add((int) (screenWidth * 59 / 100));
-		
-		//Y's for 3 Volume Icons		
-		volumeIconPosY.add((int) (screenHeight * 34 / 100) - 5);		
-		volumeIconPosY.add((int) (screenHeight * 54 / 100) - 5);		
+
+		// Y's for 3 Volume Icons
+		volumeIconPosY.add((int) (screenHeight * 34 / 100) - 5);
+		volumeIconPosY.add((int) (screenHeight * 54 / 100) - 5);
 		volumeIconPosY.add((int) (screenHeight * 74 / 100) - 5);
-		
+
 		addMouseListener(new MouseAdapter() {
 
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				
+
 				if (closeImg != null) {
-					
+
 					final Point me = e.getPoint();
 					final Rectangle bounds = new Rectangle(closeImgPosition.x, closeImgPosition.y, closeImgDimensionsX,
 							closeImgDimensionsY);
@@ -169,46 +175,46 @@ public final class Settings extends JPanel implements MenuOption {
 						frame.setVisible(false);
 					}
 				}
-			
-				if(volumeIcon != null && muteIcon != null) {
-					
+
+				if (volumeIcon != null && muteIcon != null) {
+
 					final Point me = e.getPoint();
-					
-					final Rectangle bounds0 = new Rectangle(volumeIconPosX.get(0), volumeIconPosY.get(0), volumeIconDimensionsX,
-							volumeIconDimensionsY);
-					
-					final Rectangle bounds1 = new Rectangle(volumeIconPosX.get(1), volumeIconPosY.get(1), volumeIconDimensionsX,
-							volumeIconDimensionsY);
-					
-					final Rectangle bounds2 = new Rectangle(volumeIconPosX.get(2), volumeIconPosY.get(2), volumeIconDimensionsX,
-							volumeIconDimensionsY);
-					
+
+					final Rectangle bounds0 = new Rectangle(volumeIconPosX.get(0), volumeIconPosY.get(0),
+							volumeIconDimensionsX, volumeIconDimensionsY);
+
+					final Rectangle bounds1 = new Rectangle(volumeIconPosX.get(1), volumeIconPosY.get(1),
+							volumeIconDimensionsX, volumeIconDimensionsY);
+
+					final Rectangle bounds2 = new Rectangle(volumeIconPosX.get(2), volumeIconPosY.get(2),
+							volumeIconDimensionsX, volumeIconDimensionsY);
+
 					final Sounds sounds = Sounds.getInstance();
 					boolean mustRepaint = false;
-					
-					if(bounds0.contains(me)) {
-						
+
+					if (bounds0.contains(me)) {
+
 						if (!sounds.isMenuMusicMuted())
 							sounds.stopMenuMusic();
 						else
 							sounds.loopMenuMusic();
-						
-						sounds.setMenuMusicMute(!sounds.isMenuMusicMuted());			
+
+						sounds.setMenuMusicMute(!sounds.isMenuMusicMuted());
 						mustRepaint = true;
 					}
-					
-					if(bounds1.contains(me)) {
-						
+
+					if (bounds1.contains(me)) {
+
 						sounds.setGameMusicMute(!sounds.isGameMusicMuted());
 						mustRepaint = true;
 					}
-					
-					if(bounds2.contains(me)) {
-						
+
+					if (bounds2.contains(me)) {
+
 						sounds.setBattleMusicMute(!sounds.isBattleMusicMuted());
 						mustRepaint = true;
 					}
-					
+
 					if (mustRepaint)
 						repaint();
 				}
@@ -221,7 +227,7 @@ public final class Settings extends JPanel implements MenuOption {
 
 		backgroundImg = ImageLoader.loadImage("/images/BGoption.jpg");
 		closeImg = ImageLoader.loadImage("/images/closeImg.png");
-		volumeIcon = ImageLoader.loadImage("/images/volume_icon.png");		
+		volumeIcon = ImageLoader.loadImage("/images/volume_icon.png");
 		muteIcon = ImageLoader.loadImage("/images/mute_icon.png");
 	}
 }
